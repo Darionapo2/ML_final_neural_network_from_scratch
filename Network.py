9014b3b07c669ff88fed6b6cd000f8f6d85d7599
Dario <darionapo2.0@gmail.com>
2023-12-28 18:49:32 +0100

    some changes and refactoring. added derived_sigmoid() function.

--- Network.py
+++ Network.py
@@ -2,8 +2,35 @@ from Neuron import Neuron
 class Network:
     input_neurons = []
 
-    def __init__(self, input_neurons_number: int, hidden_layers_number: int, output_neurons_number: int):
-        self.fisrt_layer_neurons = [Neuron(0) for _ in range(input_neurons_number)]
+
+    network_shape = {
+        'layers': {
+            'input': {
+                'k': 256,
+                'activation_function': 'sigmoid'
+            },
+
+            'hidden1': {
+                'n1': 10,
+                'activation_function': 'sigmoid'
+            },
+
+            'hidden2': {
+                'n2': 10,
+                'activation_function': 'sigmoid'
+            },
+
+            # ...
+
+            'output': {
+                'J': 10,
+                'activation_function': 'softmax'
+            }
+        }
+    }
+
+    def __init__(self, shape: dict):
+        pass
 
 
     def backpropagate(self):
--- utils.py
+++ utils.py
@@ -1,4 +1,9 @@
 from math import pow, e
+import numpy as np
+from numpy import typing
 
 def sigmoid(x: float) -> float:
     return 1/(1 + pow(e, -x))
+
+def derived_sigmoid(x: float) -> float:
+    return sigmoid(x) * (1 - sigmoid(x))
