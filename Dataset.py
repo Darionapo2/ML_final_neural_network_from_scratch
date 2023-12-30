a64620123c4d86338420a321330f748305f35f52
Dario <darionapo2.0@gmail.com>
2023-12-31 00:02:12 +0100

    deleted Dataset class.

--- Dataset.py
+++
@@ -1,7 +0,0 @@
-
-
-class Dataset:
-    def __init__(self, n_problem_inputs: int, n_problem_outputs: int, n_patterns: int):
-        self.k = n_problem_inputs
-        self.J = n_problem_outputs
-        self.N = n_patterns
