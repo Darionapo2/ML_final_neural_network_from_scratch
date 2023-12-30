48ca1ac38fbac955e1ab2da9de95670a8f13d5c4
Dario <darionapo2.0@gmail.com>
2023-12-31 00:04:58 +0100

    added Neuron.__str__().

--- Neuron.py
+++ Neuron.py
@@ -1,11 +1,15 @@
 from typing import Callable
 from utils import sigmoid
 
-class Neuron:
 
+class Neuron:
     activation_value: float
     bias: float
     activation_function: Callable
 
     def __init__(self, activation_function: Callable = sigmoid):
         self.activation_function = activation_function
+
+    def __str__(self):
+        return f'<Neuron({str(self.activation_function)}, value = {self.activation_value}, ' \
+               f'bias = {self.bias})>'
